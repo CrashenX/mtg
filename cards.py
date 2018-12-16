86c0f3bfcda90dc55c64e7bbf5f0874efbb12674
Jesse J. Cook <jesse@compile.fail>
2018-12-16 04:18:29 +0000

    Restructure project and update load,dump,drop

---
+++ cards.py
@@ -0,0 +1,120 @@
+# Copyright 2018 Jesse J. Cook
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# cards.py
+
+import argparse
+from pymongo import MongoClient
+from bson.json_util import loads, dumps
+from requests import get as httpget
+from requests import codes
+from sys import stdout
+
+ACTIONS = ['load', 'dump', 'drop']
+TARGETS = ['cards', 'sets', 'collection']
+ALL_TARGETS = 'everything'
+BACKUPS = 'backups/'
+
+
+def load_mtgjson(db, target):
+    emsg = ""
+    try:
+        r = httpget('https://mtgjson.com/v4/json/All%s.json' %
+                    target.capitalize())
+    except Exception as e:
+        emsg = str(e)
+    if emsg == "" and r.status_code != codes.ok:
+        emsg = "Received status code: %d" % r.status_code
+    if emsg == "":
+        for k, v in r.json().items():
+            getattr(db, target).insert_one(v).inserted_id
+    else:
+        print("Error\n\tLoading data from mtgjson.com failed: %s" % emsg)
+        print("Loading %s from '%s' instead..." % (target, BACKUPS), end='')
+        stdout.flush()
+        with open("%s/%s.json" % (BACKUPS, target), 'r') as f:
+            for i in loads(f.read()):
+                getattr(db, target).insert_one(i).inserted_id
+
+
+def load(db, args):
+    def target_load(db, target):
+        def load_cards(db, target):
+            load_mtgjson(db, target)
+
+        def load_sets(db, target):
+            load_mtgjson(db, target)
+
+        def load_collection(db, target):
+            pass
+
+        print("Loading %s..." % target, end='')
+        stdout.flush()
+        locals()["load_%s" % target](db, target)
+        print("Done")
+
+    drop(db, args)
+    if args.target == ALL_TARGETS:
+        for t in TARGETS:
+            target_load(db, t)
+    else:
+        target_load(db, args.target)
+
+
+def dump(db, args):
+    def target_dump(db, target):
+        print("Dumping %s..." % target, end='')
+        stdout.flush()
+        with open("%s/%s.json" % (BACKUPS, target), 'w') as f:
+            f.write(dumps(getattr(db, target).find({}, {"_id": 0})))
+        print("Done")
+
+    if args.target == ALL_TARGETS:
+        for t in TARGETS:
+            target_dump(db, t)
+    else:
+        target_dump(db, args.target)
+
+
+def drop(db, args):
+    def target_drop(db, target):
+        print("Dropping %s..." % target, end='')
+        stdout.flush()
+        getattr(db, target).drop()
+        print("Done")
+
+    if args.target == ALL_TARGETS:
+        for t in TARGETS:
+            target_drop(db, t)
+    else:
+        target_drop(db, args.target)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Manage MTG Collection')
+    subparsers = parser.add_subparsers(metavar='CMD')
+    for cmd in ACTIONS:
+        p = subparsers.add_parser(cmd, help='%s mtg data' % cmd)
+        tgts = TARGETS + [ALL_TARGETS]
+        p.add_argument('target',
+                       metavar='DATA',
+                       type=str,
+                       choices=tgts,
+                       help="{%s}" % '|'.join(tgts))
+        p.set_defaults(func=locals()[cmd])
+    subparsers.required = True
+    args = parser.parse_args()
+    client = MongoClient()
+    db = client.mtg
+    args.func(db, args)
